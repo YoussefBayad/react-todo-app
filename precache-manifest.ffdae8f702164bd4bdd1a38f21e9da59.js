2892b4fb5954c01d1acc8d97f1a382a4a4595a80
Joseph bayad <jobayad66@gmail.com>
2020-07-08 18:15:45 +0100

    Updates

---
+++ precache-manifest.ffdae8f702164bd4bdd1a38f21e9da59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15412a37651c445a13c90fee0dfe715b",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "7e78c3632f4a46eddc4c",
+    "url": "/react-todo-app/static/css/main.cfeef8a6.chunk.css"
+  },
+  {
+    "revision": "4bff2ba18a4eb1473ce3",
+    "url": "/react-todo-app/static/js/2.7a13645c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.7a13645c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e78c3632f4a46eddc4c",
+    "url": "/react-todo-app/static/js/main.4552b08d.chunk.js"
+  },
+  {
+    "revision": "833e946d40e701a776a3",
+    "url": "/react-todo-app/static/js/runtime-main.5291e67c.js"
+  }
+]);
